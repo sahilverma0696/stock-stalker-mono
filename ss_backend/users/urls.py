63eb52501665ec60e5c82cf4432928f8fd0c6877
Sahil Verma <sahilverma0696@gmail.com>
2023-11-12 02:13:10 +0530

    basic auth added

--- ss_backend/users/urls.py
+++ ss_backend/users/urls.py
@@ -1,7 +1,9 @@
 # users/urls.py
 from django.urls import path
 from .views import UserCreateView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    path('auth/',obtain_auth_token),
     path('signup/', UserCreateView.as_view(), name='user-create'),
 ]
