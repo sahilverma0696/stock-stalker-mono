6112f4da4c71a9d6cc6e30bd46c237a3eafbe221
Sahil Verma <sahilverma0696@gmail.com>
2023-11-14 14:29:54 +0530

    Made basic changes in WatchList APIs

--- ss_backend/internal/watchlists/views.py
+++ ss_backend/internal/watchlists/views.py
@@ -4,12 +4,18 @@ from internal.symbol.models import Symbol
 from internal.watchlists.models import WatchList
 from internal.watchlistmapping.models import WatchListMapping
 from rest_framework import permissions, authentication
+from users.models import CustomUser
 
 
+## Class to get the public lists and to make a public or a private list
 class WatchListView(APIView):
-    authentication_classes = [authentication.SessionAuthentication,
-                              authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes =[authentication.SessionAuthentication,
+                              authentication.TokenAuthentication
+                            ]
+    permission_classes =[
+                            permissions.AllowAny,
+                            permissions.IsAuthenticatedOrReadOnly,
+                        ]
 
     def get(self, request):
         # Get the list name and user ID from the request.
@@ -17,10 +23,7 @@ class WatchListView(APIView):
         user_id = request.query_params.get("user_id")
 
         # Get all the watchlists for the user, if the user ID is specified.
-        if user_id:
-            watchlists = WatchList.objects.filter(user_id=user_id)
-        else:
-            watchlists = WatchList.objects.all()
+        watchlists = WatchList.objects.all()
 
         # Create a list of JSON objects, with each object containing the watchlist name and the list of symbols.
         results = []
@@ -36,16 +39,24 @@ class WatchListView(APIView):
         # Validate the request
         watchlist = WatchList()
         watchlist.list_name = request.data.get("list_name")
-        watchlist.is_public = request.data.get("is_public", False)
+        is_public = request.data.get("is_public", False)
+    
+        # Check if the user is a staff or superuser before making a public list
+        if not request.user.is_staff and not request.user.is_superuser and is_public:
+            return Response("Permission denied: Only staff or superusers can create public watchlists.", status=403)
+    
+        watchlist.is_public = is_public
         watchlist.save()
-
+    
         symbol_list = request.data.get("symbols", [])
-
+    
         # TODO: save entry in WatchListMapping for each symbol in symbol list
         for symbol in symbol_list:
-            WatchListMapping.objects.create(
-                list_name=watchlist,
-                symbol=Symbol.objects.get(symbol=symbol),
-                user_id=request.user.id,
-            )
+            watchlistmapping = WatchListMapping()
+            watchlistmapping.list_name = watchlist
+            watchlistmapping.symbol = Symbol.objects.get(symbol=symbol)
+            watchlistmapping.user_id = CustomUser.objects.get(id=request.user.id)
+            watchlistmapping.save()
+    
         return Response("success")
+
